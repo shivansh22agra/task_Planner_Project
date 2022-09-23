e403737ceddd9ef0259ae2f6d41f4dfaeeb41b8c
shivansh22agra <agrawalshivansh22@gmail.com>
2022-09-24 03:20:46 +0530

    boiler pate code in ormconfig.ts

---
+++ task_planner_backend/src/db/config/ormconfig.ts
@@ -0,0 +1,20 @@
+// import { join, resolve } from 'path';
+
+// const options = {
+//   type: 'postgres',
+//   host: 'localhost',
+//   synchronize: true,
+//   dropSchema: false,
+//   port: 5432,
+//   username: 'postgres',
+//   password: '123456789',
+//   database: 'task_planner',
+//   entities: [resolve(__dirname, '..', 'entity', '*')],
+//   migrations: [resolve(__dirname, '..', 'migrations', '*')],
+//   cli: {
+//     entitiesDir: join('src', 'db', 'entity'),
+//     migrationsDir: join('src', 'db', 'migrations'),
+//   },
+// };
+
+// module.exports = options;
