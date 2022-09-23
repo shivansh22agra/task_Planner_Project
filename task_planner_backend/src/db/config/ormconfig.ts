c41513321cc25ec79e51e8dfe9737aac67509a34
shivansh22agra <agrawalshivansh22@gmail.com>
2022-09-24 03:21:02 +0530

    s

--- task_planner_backend/src/db/config/ormconfig.ts
+++ task_planner_backend/src/db/config/ormconfig.ts
@@ -1,20 +1,20 @@
-// import { join, resolve } from 'path';
+import { join, resolve } from 'path';
 
-// const options = {
-//   type: 'postgres',
-//   host: 'localhost',
-//   synchronize: true,
-//   dropSchema: false,
-//   port: 5432,
-//   username: 'postgres',
-//   password: '123456789',
-//   database: 'task_planner',
-//   entities: [resolve(__dirname, '..', 'entity', '*')],
-//   migrations: [resolve(__dirname, '..', 'migrations', '*')],
-//   cli: {
-//     entitiesDir: join('src', 'db', 'entity'),
-//     migrationsDir: join('src', 'db', 'migrations'),
-//   },
-// };
+const options = {
+  type: 'postgres',
+  host: 'localhost',
+  synchronize: true,
+  dropSchema: false,
+  port: 5432,
+  username: 'postgres',
+  password: '123456789',
+  database: 'task_planner',
+  entities: [resolve(__dirname, '..', 'entity', '*')],
+  migrations: [resolve(__dirname, '..', 'migrations', '*')],
+  cli: {
+    entitiesDir: join('src', 'db', 'entity'),
+    migrationsDir: join('src', 'db', 'migrations'),
+  },
+};
 
-// module.exports = options;
+module.exports = options;
